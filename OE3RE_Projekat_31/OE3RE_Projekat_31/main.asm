bf8175a89bf44754ff599efc074ac317681b56b6
Martin <macakg98@gmail.com>
2020-04-10 02:32:56 +0200

    Pocetak projekta

---
+++ OE3RE_Projekat_31/OE3RE_Projekat_31/main.asm
@@ -0,0 +1,16 @@
+; OE3RE - Projekat 31 - Parsiranje tekstualnog fajla
+; Matija Saljic
+; Martin Cvijovic
+
+.386
+.model flat, stdcall
+.stack 4096
+ ExitProcess proto, dwExitCode: dword
+.data
+		; variables are here
+.code
+	main proc
+		; code is here
+	invoke ExitProcess, 0
+	main endp
+end main
