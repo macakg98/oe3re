1b489d0db63776973eba07d0eaeac153851c11c1
Martin <macakg98@gmail.com>
2020-04-10 23:16:26 +0200

    Popravio Irvine32

--- OE3RE_Projekat_31/OE3RE_Projekat_31/main.asm
+++ OE3RE_Projekat_31/OE3RE_Projekat_31/main.asm
@@ -6,12 +6,21 @@
 .model flat, stdcall
 .stack 4096
  ExitProcess proto, dwExitCode: dword
+
+ INCLUDE Irvine32.inc
+
+ ; TODO: Kaze da ne vidi Irvine32.inc i njegove metode, svejedno ga izvrsava?
+
 .data
 		; variables are here
+		Msg1 BYTE "IRVINE32 TEST", 13, 10, 0
 .code
 	main proc
 		; code is here
-
+		mov edx, OFFSET Msg1
+		call WriteString
+	EndLoop:
+		jmp EndLoop
 	invoke ExitProcess, 0
 	main endp
 end main
