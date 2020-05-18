96ec52c9bfec8d58e72b3c62e482dbdf85c5ebd8
Martin <macakg98@gmail.com>
2020-05-18 15:35:32 +0200

    mega komit

--- OE3RE_Projekat_31/OE3RE_Projekat_31/main.asm
+++ OE3RE_Projekat_31/OE3RE_Projekat_31/main.asm
@@ -19,11 +19,11 @@
 	bytesCnt dword ? ; // koliko bajtova je procitano (broj moze biti promenljiv)
 					 ; // mora da bude uvek konstantan jer se ucitava uvek isti broj promenljivih
 					 ; // sluzi za bacanje greske ukoliko je fajl neispravno napisan
-	x0 byte ?
-	y0 byte ?
-	x1 byte ?
-	y1 byte ?
-	color byte ?
+	x0 dword ?
+	y0 dword ?
+	x1 dword ?
+	y1 dword ?
+	color dword ?
 
 	tempByte byte ?
 .data
@@ -38,6 +38,37 @@
 	infoSuccessfulRead BYTE "Uspesno procitana linija iz fajla", 13, 10, 0
 	stringEmptyLine BYTE "    ", 13, 10, 0
 .code
+	drawRect proc
+		; // matija ovde izvodis svoje magije, ako ima nesto ovde onda sam to uneo za debagovanje
+		mov ebx, 10 ; // osnova u kojoj stampamo broj mora biti upisana u EBX
+		mov eax, x0
+		call WriteInt
+		mov edx, offset stringEmptyLine
+		call WriteString
+
+		mov eax, y0
+		call WriteInt
+		mov edx, offset stringEmptyLine
+		call WriteString
+				
+		mov eax, x1
+		call WriteInt
+		mov edx, offset stringEmptyLine
+		call WriteString
+					
+		mov eax, y1
+		call WriteInt
+		mov edx, offset stringEmptyLine
+		call WriteString
+
+		mov eax, color
+		call WriteInt
+		mov edx, offset stringEmptyLine
+		call WriteString
+
+		ret
+	drawRect endp
+	
 	main proc
 		; // ucitaj ime fajla
 		mov edx, offset infoProgram1
@@ -81,6 +112,9 @@
 		mov ecx, BUFFER_SIZE  ; // max 100
 		call ReadFromFile
 
+		mov edx, offset textBuffer
+		call WriteString
+
 		mov bytesCnt, eax
 		jnc CHECKSIZE
 		jz FORCEEXIT ; // kad stignemo do EOF-a zavrsavamo
@@ -98,20 +132,96 @@
 		call WriteString
 		jmp FORCEEXIT
 
+
 	PARSERECTDATA:
 		; // mov edx, offset infoSuccessfulRead ; // debugging
 		; // call WriteString ; // 
 		; // ucitali smo jednu liniju, obradjujemo i zovemo opet dok ne dobijem
 		
 		mov eax, 0
-		mov ecx, 0
-
 		mov tempByte, 0
 
+		mov ecx, 0 ; // od 0 do 4 teramo ecx
+		           ; // 0 -> x0, 1 -> y0, 2 -> x1, 3 -> y1, 4 -> color (0-255)
+				   ; // citamo u eax, pomocu ecx znamo gde ga store-ujemo
+
+
+		mov esi, offset textBuffer
+
+	LINELOOP:
+		mov bl, [esi]
+		mov tempByte, bl
+
+		; //sub tempByte, 48
+
+		cmp tempByte, 0
+		je FORCEEXIT
+
+		cmp tempByte, '\'
+		je NEWLINE
+
+		cmp tempByte, '0'
+		jl VARCHECK
+		cmp tempByte, '9'
+		jg VARCHECK
+
+		sub tempByte, 48
+		imul eax, 10
+		add al, tempByte
+
+		inc esi
+		jmp LINELOOP
+	
+	NEWLINE: ; // ?
+		inc esi
+		jmp VARCHECK
+
+	VARCHECK:
+		cmp ecx, 0
+		je ASSIGNX0
+		cmp ecx, 1
+		je ASSIGNY0
+		cmp ecx, 2
+		je ASSIGNX1
+		cmp ecx, 3
+		je ASSIGNY1
+		cmp ecx, 4
+		je ASSIGNCOLOR 
+
+		; // do ovde ne treba da se stigne?
+
+
+	ASSIGNX0:
+		mov x0, eax
+		jmp NEXT
+	ASSIGNY0:
+		mov y0, eax
+		jmp NEXT
+	ASSIGNX1:
+		mov x1, eax
+		jmp NEXT
+	ASSIGNY1:
+		mov y1, eax
+		jmp NEXT
+	ASSIGNCOLOR:
+		mov color, eax
+		call drawRect
+
+		mov ecx, 0
+		mov eax, 0
+		mov tempByte, 0
+		inc esi
+		jmp LINELOOP
+
+	NEXT:
+		inc esi
+		mov eax, 0
+		mov tempByte, 0
+		inc ecx
+		jmp LINELOOP
 		; // x0 y0 x1 y1 color
 		; // svi su byte = 8bit, registri su 16bitni
 
-		
 	FORCEEXIT:
 		invoke ExitProcess, 0
 	main endp
